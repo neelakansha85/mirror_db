76de28da8aaa138fe69a26df5cb4118b0cf39b0a
Neel Shah <neel@nsdesigners.com>
2016-08-05 17:43:05 -0400

    added remote arg

--- db_backup/remote_db_export.sh
+++ db_backup/remote_db_export.sh
@@ -3,21 +3,29 @@
 cd ..
 . db.properties
 
-src=$1
+SRC=$1
+REMOTE=$2
 
-if [ "$src" == 'prd' ]
+if [ "$REMOTE" == 'remote']
+then
+  MYSQL_PATH='/Applications/MAMP/Library/bin/'
+else
+  MYSQL_PATH=''
+fi
+
+if [ "$SRC" == 'prd' ]
 then
   DB_USER=$prd_db_user
   DB_PASSWORD=$prd_db_pass
   DB_SCHEMA=$prd_db_name
   DB_HOST_NAME=$prd_db_host;
-elif [ "$src" == 'nyudev' ]
+elif [ "$SRC" == 'nyudev' ]
 then
   DB_USER=$dev_db_user
   DB_PASSWORD=$dev_db_pass
   DB_SCHEMA=$dev_db_name
   DB_HOST_NAME=$dev_db_host;
-elif [ "$src" == 'sswtest' ]
+elif [ "$SRC" == 'sswtest' ]
 then
   DB_USER=$sswtest_db_user
   DB_PASSWORD=$sswtest_db_pass
@@ -32,7 +40,7 @@ cd db_backup
 
 FILE_NAME_TABLST='table_list.txt'
 
-/Applications/MAMP/Library/bin/mysql --host=${DB_HOST_NAME} --user=${DB_USER} --password=${DB_PASSWORD} -A --skip-column-names -e"SELECT CONCAT(TABLE_SCHEMA,'.', TABLE_NAME) FROM information_schema.TABLES WHERE table_schema='${DB_SCHEMA}'" > ${FILE_NAME_TABLST}
+${MYSQL_PATH}mysql --host=${DB_HOST_NAME} --user=${DB_USER} --password=${DB_PASSWORD} -A --skip-column-names -e"SELECT CONCAT(TABLE_SCHEMA,'.', TABLE_NAME) FROM information_schema.TABLES WHERE table_schema='${DB_SCHEMA}'" > ${FILE_NAME_TABLST}
 
 echo "Starting to download DB... "
 now=$(date +"%T")
@@ -46,7 +54,7 @@ do
     echo "Starting to download new group... "
     DB=`echo ${DBTB} | sed 's/\./ /g' | awk '{print $1}'`
     TB=`echo ${DBTB} | sed 's/\./ /g' | awk '{print $2}'`
-    /Applications/MAMP/Library/bin/mysqldump --host=${DB_HOST_NAME}  --user=${DB_USER} --password=${DB_PASSWORD} --hex-blob --triggers ${DB} ${TB} | gzip > ${DB}_${TB}.sql.gz &
+    ${MYSQL_PATH}mysqldump --host=${DB_HOST_NAME}  --user=${DB_USER} --password=${DB_PASSWORD} --hex-blob --triggers ${DB} ${TB} | gzip > ${DB}_${TB}.sql.gz &
     (( COMMIT_COUNT++ ))
     (( TOTAL++ ))
     if [ ${COMMIT_COUNT} -eq ${COMMIT_LIMIT} ]
