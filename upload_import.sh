878e88cf6b9dcaa46aa3dd01d5734c07a28e2b25
Neel Shah <neel@nsdesigners.com>
2016-08-22 18:00:13 -0400

    Added some checks to not run sed commands if values null

--- upload_import.sh
+++ upload_import.sh
@@ -23,17 +23,23 @@ cd ${BACKUP_DIR}
 if [ -e $DB_FILE_NAME.sql ]; then
   echo "File ${DB_FILE_NAME}.sql found..."
   echo "Changing environment specific information"
-  # Replace old domain with the new domain
-  echo "Replacing Site URL..."
-  echo "Running -> sed -i'' 's/'${SRC_URL}'/'${URL}'/g' ${DB_FILE_NAME}.sql"
-  sed -i'' 's/'${SRC_URL}'/'${URL}'/g' ${DB_FILE_NAME}.sql
+  if [ ! -z $SRC_URL ]; then
+    # Replace old domain with the new domain
+    echo "Replacing Site URL..."
+    echo "Running -> sed -i'' 's/'${SRC_URL}'/'${URL}'/g' ${DB_FILE_NAME}.sql"
+    sed -i'' 's/'${SRC_URL}'/'${URL}'/g' ${DB_FILE_NAME}.sql
+  fi
 
-  # Replace Shib Production with Shib QA 
-  echo "Replacing Shibboleth URL..."
-  sed -i'' 's/'${SRC_SHIB_URL}'/'${SHIB_URL}'/g' ${DB_FILE_NAME}.sql
-  
-  echo "Replacing Google Analytics code..."
-  sed -i'' 's/'${SRC_G_ANALYTICS}'/'${G_ANALYTICS}'/g' ${DB_FILE_NAME}.sql
+  if [ ! -z $SRC_SHIB_URL ]; then
+    # Replace Shib Production with Shib QA 
+    echo "Replacing Shibboleth URL..."
+    sed -i'' 's/'${SRC_SHIB_URL}'/'${SHIB_URL}'/g' ${DB_FILE_NAME}.sql
+  fi
+
+  if [ ! -z $SRC_G_ANALYTICS ]; then
+    echo "Replacing Google Analytics code..."
+    sed -i'' 's/'${SRC_G_ANALYTICS}'/'${G_ANALYTICS}'/g' ${DB_FILE_NAME}.sql
+  fi
 fi
 
 cd ..
