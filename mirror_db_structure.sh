7ca5c33ee282f05b32b8192a75f3508453f59e8b
Neel Shah <neel@nsdesigners.com>
2016-08-22 18:49:19 -0400

    Cleaned removing of all bash files specifically

--- mirror_db_structure.sh
+++ mirror_db_structure.sh
@@ -4,6 +4,11 @@
 ARG1=$1
 REMOTE_DIR=${2:-'mirror_db'}
 BACKUP_DIR=${3:-'db_backup'}
+IMPORT_SCRIPT='import.sh'
+DROP_SQL_FILE='drop_tables'
+PARSE_FILE='parse_arguments.sh'
+READ_PROPERTIES_FILE='read_properties.sh'
+PROPERTIES_FILE='db.properties'
 
 # Move mysql.sql to archives with current date
 if [ "$ARG1" == "mk" ]; then
@@ -18,15 +23,14 @@ if [ "$ARG1" == "mk" ]; then
 	fi
 
 	# Remove all bash scripts from previous run if any
+	echo ''
 	echo "Attempting to remove all bash files if exists in ${REMOTE_DIR}"
-	rm *.sh
+	rm $IMPORT_SCRIPT $PARSE_FILE $READ_PROPERTIES_FILE $PROPERTIES_FILE $DROP_SQL_FILE
 
 	#chmod 775 $REMOTE_DIR $REMOTE_DIR/$BACKUP_DIR
 
 elif [ "$ARG1" == "rm" ]; then
 	cd $REMOTE_DIR
-	rm *.sh
-	rm *.properties
-	rm drop_tables.sql
+	rm $IMPORT_SCRIPT $PARSE_FILE $READ_PROPERTIES_FILE $PROPERTIES_FILE $DROP_SQL_FILE
 fi
 
