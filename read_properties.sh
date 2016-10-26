0f56cecea5d5ab3121cf8ddfd25270d664ca50c5
Neel Shah <neel@nsdesigners.com>
2016-10-26 15:38:10 -0400

    Updated read_properties for correct server references.

--- read_properties.sh
+++ read_properties.sh
@@ -9,86 +9,59 @@ if [ "$SRC" == 'prd' ]; then
   DB_PASSWORD=$prd_db_pass
   DB_HOST_NAME=$prd_db_host
   DB_SCHEMA=$prd_db_name
-  G_ANALYTICS=$prd_g_analytics
   URL=$prd_url
+  HOST_NAME=$prd_host
+  SITE_DIR=$prd_dir
+  REMOTE_SCRIPT_DIR=$prd_remote_dir
   SHIB_URL=$prd_shib_url
+  SHIB_LOGOUT_URL=$prd_shib_logout_url
+  G_ANALYTICS=$prd_g_analytics
 
-elif [ "$SRC" == 'nyudev' ]; then
+elif [ "$SRC" == 'dev' ]; then
   DB_USER=$dev_db_user
   DB_PASSWORD=$dev_db_pass
   DB_HOST_NAME=$dev_db_host
   DB_SCHEMA=$dev_db_name
-  G_ANALYTICS=$dev_g_analytics
   URL=$dev_url
+  HOST_NAME=$dev_host
+  SITE_DIR=$dev_db_dir
+  REMOTE_SCRIPT_DIR=$dev_remote_dir
   SHIB_URL=$dev_shib_url
+  SHIB_LOGOUT_URL=$dev_shib_logout_url
+  G_ANALYTICS=$dev_g_analytics
+  SUPERADMIN=$dev_superadmin
 
-elif [ "$SRC" == 'nyuupdates' ]; then
-  DB_USER=$nyuupdates_db_user
-  DB_PASSWORD=$nyuupdates_db_pass
-  DB_HOST_NAME=$nyuupdates_db_host
-  DB_SCHEMA=$nyuupdates_db_name
-  G_ANALYTICS=$nyuupdates_g_analytics
-  URL=$nyuupdates_url
-  SHIB_URL=$nyuupdates_shib_url
+elif [ "$SRC" == 'dev2' ]; then
+  DB_USER=$dev2_db_user
+  DB_PASSWORD=$dev2_db_pass
+  DB_HOST_NAME=$dev2_db_host
+  DB_SCHEMA=$dev2_db_name
+  URL=$dev2_url
+  HOST_NAME=$dev2_host
+  SITE_DIR=$dev2_db_dir
+  REMOTE_SCRIPT_DIR=$dev2_remote_dir
+  SHIB_URL=$dev2_shib_url
+  SHIB_LOGOUT_URL=$dev2_shib_logout_url
+  G_ANALYTICS=$dev2_g_analytics
+  SUPERADMIN=$dev2_superadmin
 
-elif [ "$SRC" == 'sswtest' ]; then
-  DB_USER=$sswtest_db_user
-  DB_PASSWORD=$sswtest_db_pass
-  DB_HOST_NAME=$sswtest_db_host
-  DB_SCHEMA=$sswtest_db_name
-  G_ANALYTICS=$sswtest_g_analytics
-  URL=$sswtest_url
-  SHIB_URL=$sswtest_shib_url
+elif [ "$SRC" == 'qa' ]; then
+  DB_USER=$qa_db_user
+  DB_PASSWORD=$qa_db_pass
+  DB_HOST_NAME=$qa_db_host
+  DB_SCHEMA=$qa_db_name
+  URL=$qa_url
+  HOST_NAME=$qa_host
+  SITE_DIR=$qa_db_dir
+  REMOTE_SCRIPT_DIR=$qa_remote_dir
+  SHIB_URL=$qa_shib_url
+  SHIB_LOGOUT_URL=$qa_shib_logout_url
+  G_ANALYTICS=$qa_g_analytics
+  SUPERADMIN=$qa_superadmin
 
-elif [ "$SRC" == 'wptst' ]; then
-  DB_USER=$wptst_db_user
-  DB_PASSWORD=$wptst_db_pass
-  DB_HOST_NAME=$wptst_db_host
-  DB_SCHEMA=$wptst_db_name
-  G_ANALYTICS=$wptst_g_analytics
-  URL=$wptst_url
-  SHIB_URL=$wptst_shib_url
-
-elif [ "$SRC" == 'pagely_prd' ]; then
-  DB_USER=$pagely_prd_db_user
-  DB_PASSWORD=$pagely_prd_db_pass
-  DB_HOST_NAME=$pagely_prd_db_host
-  DB_SCHEMA=$pagely_prd_db_name
-  G_ANALYTICS=$pagely_prd_g_analytics
-  URL=$pagely_prd_url # Value for URL replacement
-  HOST_NAME=$pagely_prd_host
-  SITE_DIR=$pagely_prd_dir
-  SHIB_URL=$pagely_prd_shib_url
-  REMOTE_SCRIPT_DIR=$pagely_prd_remote_dir
-
-elif [ "$SRC" == 'pagely_test_prd' ]; then
-  DB_USER=$pagely_test_prd_db_user
-  DB_PASSWORD=$pagely_test_prd_db_pass
-  DB_HOST_NAME=$pagely_test_prd_db_host
-  DB_SCHEMA=$pagely_test_prd_db_name
-  G_ANALYTICS=$pagely_test_prd_g_analytics
-  URL=$pagely_test_prd_url # Value for URL replacement
-  HOST_NAME=$pagely_test_prd_host
-  SITE_DIR=$pagely_test_prd_dir
-  SHIB_URL=$pagely_test_prd_shib_url
-  SUPERADMIN=$pagely_test_prd_superadmin
-  REMOTE_SCRIPT_DIR=$pagely_test_prd_remote_dir
-
-elif [ "$SRC" == 'pagely_dev' ]; then
-	DB_USER=$pagely_dev_db_user
-	DB_PASSWORD=$pagely_dev_db_pass
-	DB_HOST_NAME=$pagely_dev_db_host
-  DB_SCHEMA=$pagely_dev_db_name
-	URL=$pagely_dev_url # Value for URL replacement
-  G_ANALYTICS=$pagely_dev_g_analytics
-  HOST_NAME=$pagely_dev_host
-  SITE_DIR=$pagely_dev_db_dir
-  SHIB_URL=$pagely_dev_shib_url
-  SUPERADMIN=$pagely_dev_superadmin
-  REMOTE_SCRIPT_DIR=$pagely_dev_remote_dir
 else
-	echo "Source incorrectly specified"
-	exit 1;
+  echo "Source incorrectly specified"
+  exit 1;
 fi
 
 # Set SSH Parameters
@@ -100,12 +73,13 @@ export DB_PASSWORD
 export DB_HOST_NAME
 export DB_SCHEMA
 export URL
-export G_ANALYTICS
 export HOST_NAME
 export SITE_DIR
-export SHIB_URL
-export SUPERADMIN
 export REMOTE_SCRIPT_DIR
+export SHIB_URL
+export SHIB_LOGOUT_URL
+export G_ANALYTICS
+export SUPERADMIN
 
 export SSH_KEY_PATH
 export SSH_USERNAME
