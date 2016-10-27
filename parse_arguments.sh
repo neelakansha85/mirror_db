f0cf74b6abfee0e104db0fea96aa74aa67a1f709
Neel Shah <neel@nsdesigners.com>
2016-10-27 15:27:14 -0400

    Updated parsge_arguments.sh with case for improvement

--- parse_arguments.sh
+++ parse_arguments.sh
@@ -1,115 +1,115 @@
 #!/bin/bash
-myarray=("$@")
 
-if [ $# -ge 2 ]; then
-
-    for (( i=0; i < $# ; i++ )); 
-    do 
-        if [ "${myarray[i]}" == "-s" ]; then
-            i=`expr ${i}+1`
-            SRC=${myarray[i]} 
-
-        elif [ "${myarray[i]}" == "-d" ]; then
-            i=`expr ${i}+1`
-            DEST=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "-ebl" ]; then
-            i=`expr ${i}+1`
-            BATCH_LIMIT=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "-pl" ]; then
-            i=`expr ${i}+1`
-            POOL_LIMIT=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "-mbl" ]; then
-            i=`expr ${i}+1`
-            MERGE_BATCH_LIMIT=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "-ewt" ]; then
-            i=`expr ${i}+1`
-            WAIT_TIME=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "-iwt" ]; then
-            i=`expr ${i}+1`
-            IMPORT_WAIT_TIME=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "-lf" ]; then
-            i=`expr ${i}+1`
-            LIST_FILE_NAME=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "-dbf" ]; then
-            i=`expr ${i}+1`
-            DB_FILE_NAME=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "--site-url" ]; then
-            i=`expr ${i}+1`
-            SRC_URL=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "--shib-url" ]; then
-            i=`expr ${i}+1`
-            SRC_SHIB_URL=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "--g-analytics" ]; then
-            i=`expr ${i}+1`
-            SRC_G_ANALYTICS=${myarray[i]}
-
-        elif [ "${myarray[i]}" == "--force" ]; then
-            FORCE_IMPORT='--force'
-
-        elif [ "${myarray[i]}" == "--drop-tables" ]; then
-            DROP_TABLES=true
-
-        elif [ "${myarray[i]}" == "--drop-tables-sql" ]; then
-            DROP_TABLES_SQL=true
-
-        elif [ "${myarray[i]}" == "--skip-export" ]; then
-            SKIP_EXPORT=true
-
-        elif [ "${myarray[i]}" == "--skip-import" ]; then
-            SKIP_IMPORT=true
-
-        elif [ "${myarray[i]}" == "--skip-network-import" ]; then
-            SKIP_NETWORK_IMPORT=true
-
-        elif [ "${myarray[i]}" == "--skip-replace" ]; then
-            SKIP_REPLACE=true
-        
-        elif [ "${myarray[i]}" == "--parallel-import" ]; then
-            PARALLEL_IMPORT=true
-
-        elif [ "${myarray[i]}" == "--is-last-import" ]; then
-            IS_LAST_IMPORT=true
-
-        else
-            echo "Please select correct option"
-            exit 1
-        fi
-    done
-
-    export SRC
-    export DEST
-    export BATCH_LIMIT
-    export POOL_LIMIT
-    export MERGE_BATCH_LIMIT
-    export WAIT_TIME
-    export IMPORT_WAIT_TIME
-    export LIST_FILE_NAME
-    export DB_FILE_NAME
-    export SRC_URL
-    export SRC_SHIB_URL
-    export SRC_G_ANALYTICS
-    export FORCE_IMPORT
-    export DROP_TABLES
-    export DROP_TABLES_SQL
-    export SKIP_EXPORT
-    export SKIP_IMPORT
-    export SKIP_NETWORK_IMPORT
-    export SKIP_REPLACE
-    export PARALLEL_IMPORT
-    export IS_LAST_IMPORT
-    
-    return
-else
-    echo "Please enter compulsory arguments"
+if [ ! $# -ge 2 ]; then
+    echo "Please enter the source or destination to run"
     exit 1
 fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+      -s | --source)
+        SRC=$2
+        shift
+        ;;
+      -d | --destination)
+        DEST=$2
+        shift
+        ;;
+      -ebl )
+        BATCH_LIMIT=$2
+        shift
+        ;;
+      -pl )
+        POOL_LIMIT=$2
+        shift
+        ;;
+      -mbl )
+        MERGE_BATCH_LIMIT=$2
+        shift
+        ;;
+      -ewt )
+        WAIT_TIME=$2
+        shift
+        ;;
+      -iwt )
+        IMPORT_WAIT_TIME=$2
+        shift
+        ;;
+      -lf )
+        LIST_FILE_NAME=$2
+        shift
+        ;;
+      -dbf )
+        DB_FILE_NAME=$2
+        shift
+        ;;
+      --site-url )
+        SRC_URL=$2
+        shift
+        ;;
+      --shib-url )
+        SRC_SHIB_URL=$2
+        shift
+        ;;
+      --g-analytics )
+        SRC_G_ANALYTICS=$
+        ;;
+      --force )
+        FORCE_IMPORT=--force
+        ;;
+      --drop-tables)
+        DROP_TABLES=true
+        ;;
+      --drop-tables-sql)
+        DROP_TABLES_SQL=true
+        ;;
+      --skip-export)
+        SKIP_EXPORT=true
+        ;;
+      --skip-import)
+        SKIP_IMPORT=true
+        ;;
+      --skip-network-import)
+        SKIP_NETWORK_IMPORT=true
+        ;;
+      --skip-replace)
+        SKIP_REPLACE=true
+        ;;
+      --parallel-import)
+        PARALLEL_IMPORT=true
+        ;;
+      --is-last-import)
+        IS_LAST_IMPORT=true
+        ;;
+      -- ) 
+        shift; 
+        break 
+        ;;
+      * ) 
+        break 
+        ;;
+    esac
+    shift
+done
+
+export SRC
+export DEST
+export BATCH_LIMIT
+export POOL_LIMIT
+export MERGE_BATCH_LIMIT
+export WAIT_TIME
+export IMPORT_WAIT_TIME
+export LIST_FILE_NAME
+export DB_FILE_NAME
+export SRC_URL
+export SRC_SHIB_URL
+export SRC_G_ANALYTICS
+export FORCE_IMPORT
+export DROP_TABLES
+export DROP_TABLES_SQL
+export SKIP_EXPORT
+export SKIP_IMPORT
+export SKIP_NETWORK_IMPORT
+export SKIP_REPLACE
+export PARALLEL_IMPORT
+export IS_LAST_IMPORT
