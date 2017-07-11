7b5d2eb9ff05f3699b1c89be14254b92dbed52f7
Neel Shah <neel@nsdesigners.com>
2017-07-11 18:30:38 -0400

    removed references to old files and their variables.

--- mirror_db_structure.sh
+++ mirror_db_structure.sh
@@ -5,12 +5,8 @@ mirrorDbStructure() {
   ARG1=$1
   EXPORT_DIR=${2:-'db_export'}
   IMPORT_SCRIPT='import.sh'
-  SEARCH_REPLACE_SCRIPT='search_replace.sh'
-  AFTER_IMPORT_SCRIPT='after_import.sh'
   DROP_SQL_FILE='drop_tables'
   SUPER_ADMIN_TXT='superadmin_dev.txt'
-  PARSE_FILE='parse_arguments.sh'
-  READ_PROPERTIES_FILE='read_properties.sh'
   PROPERTIES_FILE='db.properties'
 
   if [ "$ARG1" == "mk" ]; then
@@ -25,10 +21,10 @@ mirrorDbStructure() {
 
 	  # Remove all bash scripts from previous run if any
 	  echo "Attempting to remove all old script files if exists on server"
-	  rm -f $IMPORT_SCRIPT $SEARCH_REPLACE_SCRIPT $PARSE_FILE $READ_PROPERTIES_FILE $PROPERTIES_FILE $DROP_SQL_FILE.sql ${SUPER_ADMIN_TXT} ${AFTER_IMPORT_SCRIPT}
+	  rm -f $IMPORT_SCRIPT $PROPERTIES_FILE $DROP_SQL_FILE.sql $SUPER_ADMIN_TXT
 
   elif [ "$ARG1" == "rm" ]; then
-	  rm -f $IMPORT_SCRIPT $SEARCH_REPLACE_SCRIPT $PARSE_FILE $READ_PROPERTIES_FILE $PROPERTIES_FILE $DROP_SQL_FILE.sql ${SUPER_ADMIN_TXT} ${AFTER_IMPORT_SCRIPT}
+	  rm -f $IMPORT_SCRIPT $PROPERTIES_FILE $DROP_SQL_FILE.sql $SUPER_ADMIN_TXT
   fi
 }
 
