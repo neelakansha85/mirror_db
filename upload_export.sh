83b86c46943b6e85293619efddf34cb1ea60f35a
Garima Negi <garima.negy@gmail.com>
2017-07-12 16:53:57 -0400

    replaced value of PI by flag while calling export

--- upload_export.sh
+++ upload_export.sh
@@ -17,7 +17,7 @@ uploadExportMain() {
     echo "Start time : $now "
     uploadMirrorDbFiles $SRC
     # TODO: Use screen for waiting while SRC performs export to avoid broken pipe
-    ssh -i ${SSH_KEY_PATH} ${SSH_USERNAME}@${HOST_NAME} "cd ${REMOTE_SCRIPT_DIR}; ./${EXPORT_SCRIPT} -s ${SRC} -d ${DEST} -ebl ${BATCH_LIMIT} -pl ${POOL_LIMIT} -mbl ${MERGE_BATCH_LIMIT} -ewt ${WAIT_TIME} -lf ${LIST_FILE_NAME} -dbf ${DB_FILE_NAME} ${PARALLEL_IMPORT} ${NETWORK_FLAG} --blog-id ${BLOG_ID};"
+    ssh -i ${SSH_KEY_PATH} ${SSH_USERNAME}@${HOST_NAME} "cd ${REMOTE_SCRIPT_DIR}; ./${EXPORT_SCRIPT} -s ${SRC} -d ${DEST} -ebl ${BATCH_LIMIT} -pl ${POOL_LIMIT} -mbl ${MERGE_BATCH_LIMIT} -ewt ${WAIT_TIME} -lf ${LIST_FILE_NAME} -dbf ${DB_FILE_NAME} --parallel-import --network-flag --blog-id ${BLOG_ID};"
 
     if ( ssh -i ${SSH_KEY_PATH} ${SSH_USERNAME}@${HOST_NAME} "[ -d ${DB_BACKUP_DIR} ]" ); then
       # Get path for source db relative to DB_BACKUP_DIR
