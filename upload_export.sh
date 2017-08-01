785c1148e63a530f85e75c99e01fa0ebc5a6225d
Garima Negi <garima.negy@gmail.com>
2017-08-01 17:40:51 -0400

    Added code to start screen with name ‘export and terminate it after export.sh script has completed running

--- upload_export.sh
+++ upload_export.sh
@@ -20,8 +20,13 @@ uploadExportMain() {
     now=$(date +"%T")
     echo "Start time : $now "
     uploadMirrorDbFiles $SRC
+
     # TODO: Use screen for waiting while SRC performs export to avoid broken pipe
+    $(screen -D -R 'export')
     ssh -i ${SSH_KEY_PATH} ${SSH_USERNAME}@${HOST_NAME} "cd ${REMOTE_SCRIPT_DIR}; ./${EXPORT_SCRIPT} -s ${SRC} -d ${DEST} -ebl ${BATCH_LIMIT} -pl ${POOL_LIMIT} -mbl ${MERGE_BATCH_LIMIT} -ewt ${WAIT_TIME} -lf ${LIST_FILE_NAME} -dbf ${DB_FILE_NAME} ${networkFlag} --blog-id ${BLOG_ID};"
+    # $(exit)
+    # kill :
+    $(screen -S export -X quit)
 
     # Get path for source db relative to DB_BACKUP_DIR
     DB_FILE_N=$(getFileName $DB_FILE_NAME)
